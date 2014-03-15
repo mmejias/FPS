6d274cbde775294cde62510e3e3a1aff34f4c56f
GalacticOne <holtremington@gmail.com>
2014-03-15 01:22:20 -0400

    "Working Observer interface"

--- Subject.h
+++ Subject.h
@@ -1,16 +1,30 @@
-#include "Observer.h"
-
 #ifndef SUBJECT_H
 #define SUBJECT_H
+
+#include <stdio.h>
+#include "Observer.h"
 
 class Subject
 {
     public:
-        virtual void registerObserver(Observer) = 0;
-        virtual void unregisterObserver(Observer) = 0;
-        virtual void notifyObservers() = 0;
-    private:
-        Observer observers[100];
+        virtual void registerObserver(Observer);
+        virtual void unregisterObserver(Observer);
+        virtual void notifyObservers();
 };
+
+void Subject::registerObserver(Observer o)
+{
+    
+}
+
+void Subject::unregisterObserver(Observer o)
+{
+
+}
+
+void Subject::notifyObservers()
+{
+
+}
 
 #endif
